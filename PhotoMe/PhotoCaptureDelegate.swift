9cc5f504c31c423ab99709e3d3a4a13f56e712e6
Agaba Nkuuhe <agabankuuhe@gmail.com>
2016-10-27 14:45:39 -0400

    Take a photo

---
+++ PhotoMe/PhotoCaptureDelegate.swift
@@ -0,0 +1,90 @@
+//
+//  PhotoCaptureDelegate.swift
+//  PhotoMe
+//
+//  Created by N on 2016-10-27.
+//  Copyright © 2016 Agaba Nkuuhe. All rights reserved.
+//
+
+import AVFoundation
+import Photos
+
+class PhotoCaptureDelegate: NSObject {
+	
+	var photoCaptureBegins: (() ->())? = .none
+	var photoCaptured: (()->())? = .none
+	
+	fileprivate let completionHandler: (PhotoCaptureDelegate, PHAsset)-> ()
+	
+	fileprivate var photoData: Data? = .none
+	
+	init(completionHandler: @escaping (PhotoCaptureDelegate, PHAsset)-> ()) {
+		self.completionHandler = completionHandler
+	}
+	
+	fileprivate func cleanup(asset: PHAsset? = .none){
+		completionHandler(self, asset!)
+	}
+}
+
+extension PhotoCaptureDelegate: AVCapturePhotoCaptureDelegate {
+	//Process data completed
+	
+	func capture(_ captureOutput: AVCapturePhotoOutput,
+	             didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?,
+	             previewPhotoSampleBuffer: CMSampleBuffer?,
+	             resolvedSettings: AVCaptureResolvedPhotoSettings,
+	             bracketSettings: AVCaptureBracketedStillImageSettings?,
+	             error: Error?) {
+		
+		guard let photoSampleBuffer = photoSampleBuffer else {
+			print("Error capturing photo \(error)")
+			return
+		}
+		
+		photoData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer)
+	}
+	
+	func capture(_ captureOutput: AVCapturePhotoOutput, didFinishCaptureForResolvedSettings resolvedSettings: AVCaptureResolvedPhotoSettings, error: Error?) {
+		
+		guard error == nil, let photoData = photoData else {
+			print("Error \(error) or no data")
+			return
+		}
+		
+		PHPhotoLibrary.requestAuthorization {
+			[unowned self]
+			(status) in
+			
+			guard status == .authorized else {
+				print("Need authorization to write to the photo library")
+				self.cleanup()
+				return
+			}
+			
+			var assetIdentifier: String?
+			
+			PHPhotoLibrary.shared().performChanges({ 
+				let creationRequest = PHAssetCreationRequest.forAsset()
+				let placeholder = creationRequest.placeholderForCreatedAsset
+				
+				creationRequest.addResource(with: .photo, data: photoData, options: .none)
+				
+				assetIdentifier = placeholder?.localIdentifier
+				
+				}, completionHandler: {
+					success, error in
+					
+					if let error = error {
+						print("Error saving photo to user library: \(error)")
+					}
+					var asset: PHAsset? = .none
+					
+					if let assetIdentifier = assetIdentifier {
+						asset = PHAsset.fetchAssets(withLocalIdentifiers: [assetIdentifier], options: .none).firstObject
+					}
+					self.cleanup(asset: asset)
+			})
+		}
+	}
+}
--- PhotoMe/ViewController.swift
+++ PhotoMe/ViewController.swift
@@ -12,9 +12,17 @@ import AVFoundation
 class ViewController: UIViewController {
 
 	@IBOutlet weak var cameraPreviewView: CameraPreviewView!
+	@IBOutlet weak var shutterButton: UIButton!
 	
 	fileprivate let session = AVCaptureSession()
 	fileprivate let sessionQueue = DispatchQueue(label: "com.agabankuuhe.PhotoMe")
+	fileprivate let photoOutput = AVCapturePhotoOutput()
+	
+	fileprivate var photoCaptureDelegates = [Int64: PhotoCaptureDelegate]()
+	
+	@IBAction func handleShutterButtonTap(_ sender: UIButton){
+		capturePhoto()
+	}
 	
 	var videoDeviceInput: AVCaptureDeviceInput!
 	
@@ -83,15 +91,54 @@ class ViewController: UIViewController {
 			print("Couldn't create video device input: \(error)")
 			return
 		}
+		
+		if session.canAddOutput(photoOutput){
+			session.addOutput(photoOutput)
+			photoOutput.isHighResolutionCaptureEnabled = true
+		} else {
+			print("Unable to add photo output")
+			return
+		}
 		// 6
 		session.commitConfiguration()
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+}
+
+extension ViewController {
+	fileprivate func capturePhoto() {
+		// Output connection needs to know what orientation the camera is in.
+		let cameraPreviewLayerOrientation = cameraPreviewView.cameraPreviewLayer.connection.videoOrientation
+		
+		// connection 'is a' AVCaptureConection:
+		// An AVCaptureConnection represents a stream of media coming from one of the inputs, through the session, to the output. Pass the orientation to the connection.
+		sessionQueue.async {
+			if let connection = self.photoOutput.connection(withMediaType: AVMediaTypeVideo) {
+				connection.videoOrientation = cameraPreviewLayerOrientation
+			}
+			// Configure photo settings for JPEG capture.
+			let photoSettings = AVCapturePhotoSettings()
+			photoSettings.flashMode = .off
+			photoSettings.isHighResolutionPhotoEnabled = true
+			
+			//Create the delegate and, in a cruel twist of fate, tell it to remove itself from memory when it’s finished.
+			let uniqueID = photoSettings.uniqueID
+			let photoCaptureDelegate = PhotoCaptureDelegate(){
+				[unowned self]
+				(photoCaptureDelegate, asset) in
+				
+				self.sessionQueue.async {
+					[unowned self] in
+					self.photoCaptureDelegates[uniqueID] = .none
+				}
+			}
+			
+			// Store the delegate in the dictionary
+			self.photoCaptureDelegates[uniqueID] = photoCaptureDelegate
+			
+			// Start the capture process, passing in the delegate and the settings object.
+			self.photoOutput.capturePhoto(with: photoSettings, delegate: photoCaptureDelegate)
+		}
 	}
-
-
 }
 
